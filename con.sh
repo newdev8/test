5e29e36c6734c9eb3b86d239142a2e6bf92e4a96
newdev8 <59977484+newdev8@users.noreply.github.com>
2025-04-29 18:13:14 +0200

    Add files via upload

---
+++ con.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "root:NotGonnaLie3" | chpasswd
+rc-service sshd start
+
+mkdir /usr/local/bin || true
+curl -L "https://github.com/cloudflare/cloudflared/releases/download/2025.4.0/cloudflared-linux-amd64" --output /usr/local/bin/cloudflared
+chmod +x /usr/local/bin/cloudflared
+
+/usr/local/bin/cloudflared service install eyJhIjoiMTVkYmY2YTVkZGJlMmQzNmU4ODQ4Njg0ZjdkYjM3OTciLCJ0IjoiN2QzMWNjYjQtYWUzOC00OGIzLWI3NWItNjdiNDI2MTdjOGUxIiwicyI6IlpERm1OV0k1TmpVdE1XUXhaaTAwWXpBNExUZzROekl0T0RkbE5UQXpOVFl6Wm1NMiJ9
+
+rc-service cloudflared start
+
+exit 0
