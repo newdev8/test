a5e4c5406b4828e5f071dd62164984e1ac4f4360
newdev8 <59977484+newdev8@users.noreply.github.com>
2025-04-29 18:16:51 +0200

    Update con.sh

--- con.sh
+++ con.sh
@@ -7,7 +7,7 @@ mkdir /usr/local/bin || true
 curl -L "https://github.com/cloudflare/cloudflared/releases/download/2025.4.0/cloudflared-linux-amd64" --output /usr/local/bin/cloudflared
 chmod +x /usr/local/bin/cloudflared
 
-/usr/local/bin/cloudflared service install eyJhIjoiMTVkYmY2YTVkZGJlMmQzNmU4ODQ4Njg0ZjdkYjM3OTciLCJ0IjoiN2QzMWNjYjQtYWUzOC00OGIzLWI3NWItNjdiNDI2MTdjOGUxIiwicyI6IlpERm1OV0k1TmpVdE1XUXhaaTAwWXpBNExUZzROekl0T0RkbE5UQXpOVFl6Wm1NMiJ9
+/usr/local/bin/cloudflared service install eyJhIjoiMTVkYmY2YTVkZGJlMmQzNmU4ODQ4Njg0ZjdkYjM3OTciLCJ0IjoiN2QzMWNjYjQtYWUzOC00OGIzLWI3NWItNjdiNDI2MTdjOGUxIiwicyI6IlpERm1OV0k1TmpVdE1XUXhaaTAwWXpBNExUZzROekl0T0RkbE5UQXpOVFl6Wm1NMiJ9 || true
 
 rc-service cloudflared start
 
